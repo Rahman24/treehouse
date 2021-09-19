4500faeb72a1d88d314a4d4c01c0bbae76428835
Rahman24 <50796694+Rahman24@users.noreply.github.com>
2021-09-19 15:39:38 +0530

    ui fixes (#6)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,12 +10,13 @@ const Footer = () => {
   return (
     <footer className="footer ">
       <hr className="line" />
+      <p style={{ fontSize: "1rem", color:"rgba(255,255,255,0.6)", textAlign: "center" }}>This website is maintained by the students of IITM BSc Program and the contents in no way represent the Institute's Views and Opinions</p>
       <div className="row">
         <div className="col-6 contact-us-section">
           <ul className="list-group list-group-horizontal">
             <li className="list-group-item bg-transparent border-0 px-0">
               <a
-                href="https://www.instagram.com/invictus_iitm/"
+                href="https://www.instagram.com/iitmadras_bsc"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -24,7 +25,7 @@ const Footer = () => {
             </li>
             <li className="list-group-item bg-transparent border-0 px-0">
               <a
-                href="https://www.youtube.com/channel/UCax6gDzWhjc1177nGU2A_Ew"
+                href="https://www.youtube.com/channel/UCvKzzGO37oT83K0FwnUucxw"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -45,7 +46,7 @@ const Footer = () => {
         <div className="col-6 text-end logo">
           <ul className="list-group list-group-horizontal">
             <li className="list-group-item bg-transparent border-0 px-0">
-              <img src={logo} alt="wnk logo" />
+              <img src={logo} alt="treehouse logo" />
             </li>
           </ul>
         </div>
